e56224414ae1e2603918e7f336b0c79e11afb601
Naveen <naveengorakala814@gmail.com>
2023-02-01 20:39:44 -0800

    Add: Day2 practice problems added

---
+++ Day2/Helloworld.java
@@ -0,0 +1,12 @@
+class Helloworld{
+   public static void main(String[] args){
+      while(true){
+         System.out.println("Hello Terminal");
+         try{
+            Thread.sleep(5000);
+            }
+         catch(Exception e){
+}
+
+}
+}}
