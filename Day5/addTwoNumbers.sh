9c2dcf9332c52aa67d3382063409e7ba5b3d185f
Naveen <naveengorakala814@gmail.com>
2023-02-03 09:59:33 -0800

    Add: Day5 practiceproblems added

---
+++ Day5/addTwoNumbers.sh
@@ -0,0 +1,5 @@
+num1=678
+num2=876
+sum=$(($num1+$num2))
+echo "sum is:"$sum
+
---
+++ Day5/addTwoNumbersUserInput.sh
@@ -0,0 +1,5 @@
+read -p " Enter 1st Num: " Num1
+read -p " Enter 2nd Num: " Num2
+sum=$(($Num1+$Num2))
+echo "Addition:" $sum  
+
